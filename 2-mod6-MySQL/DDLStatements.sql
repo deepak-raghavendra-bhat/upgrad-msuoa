da8c1feb98f471943be97da46b90887c4d548e40
deepakraghavendrabhat <deepak.raghavendra.bhat@outlook.com>
2023-03-06 19:15:49 +0530

    Create DDLStatements.sql

---
+++ 2-mod6-MySQL/DDLStatements.sql
@@ -0,0 +1,20 @@
+-- create database
+
+create database demonstration;
+
+-- show al databases
+show databases;
+
+-- use the specific database
+use demonstration;
+
+-- create table
+
+create table demo_table(
+	id int not null,
+    fullname varchar(32)
+)
+
+-- insert data
+
+insert into 

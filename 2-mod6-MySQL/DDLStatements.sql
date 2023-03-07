57bc6ca9b8e26b45c53092432caf68323152399b
deepakraghavendrabhat <deepak.raghavendra.bhat@outlook.com>
2023-03-07 14:23:21 +0530

    DDL Statements Session

--- 2-mod6-MySQL/DDLStatements.sql
+++ 2-mod6-MySQL/DDLStatements.sql
@@ -1,20 +1,42 @@
--- create database
+-- DDL 
+
+-- create database 
 
 create database demonstration;
 
--- show al databases
-show databases;
+-- using DATABASE
 
--- use the specific database
 use demonstration;
 
--- create table
+-- create TABLE
 
-create table demo_table(
-	id int not null,
-    fullname varchar(32)
-)
+create TABLE demo_table (fn VARCHAR(10), id INT not null, ln VARCHAR(10));
 
--- insert data
+desc demo_table;
 
-insert into 
+-- alter TABLE
+
+-- add primary KEY
+
+alter table demo_table add constraint primary key (id);
+
+-- add new COLUMNS
+
+alter table demo_table add COLUMN age int;
+
+desc demo_table;
+
+-- drop commands
+
+-- drop COLUMNS
+
+alter table demo_table drop column age;
+
+-- dropping TABLE
+
+drop table demo_table;
+
+-- drop Db
+
+drop DATABASE demonstration;
+

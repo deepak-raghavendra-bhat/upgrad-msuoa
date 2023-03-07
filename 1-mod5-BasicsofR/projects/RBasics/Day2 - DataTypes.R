45436904c2fad3e49b225addfb966160cfb3c30a
deepakraghavendrabhat <deepak.raghavendra.bhat@outlook.com>
2023-03-07 23:07:50 +0530

    Datatypes and DS

---
+++ 1-mod5-BasicsofR/projects/RBasics/Day2 - DataTypes.R
@@ -0,0 +1,25 @@
+my_Var <- 4
+my_Var
+
+count <- 10
+count
+typeof(count)
+
+count + 4
+
+boolean = TRUE
+boolean
+typeof(boolean)
+
+?is.na
+
+string = "There are 10 dogs"
+string
+
+as.double('10')
+
+str1 = "There"
+str2 = "are"
+paste(str1,str2,sep = ",")
+paste0(str1,str2)
+paste(str1,str2)
---
+++ 1-mod5-BasicsofR/projects/RBasics/Day3- DS - P1.R
@@ -0,0 +1,36 @@
+# vectors
+
+1:10
+c(1:10)
+c(TRUE,FALSE)
+
+# dog breeds
+breeds <- c("A","B","C")
+fur_color <- c("D","E","F")
+age <- c(1,2,3)
+
+breeds[1]
+
+age[1] = 3
+age
+
+length(age)
+c(breeds,"G")
+breeds
+
+age + 4
+c(1:3, 5:9)
+
+breeds[age > 2]
+
+# lists
+
+c("Lab",7)
+list("Lab",7)
+
+mix_list <- list(breeds,age)
+names(mix_list) <- c("Breeds","Age")
+mix_list[[1]]
+
+mix_list["Breeds"]
+         
